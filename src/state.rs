caa160e293fd9484ecfb9324b869ddf652a6c958
Reyth3 <mrreynevan2@gmail.com>
2024-11-13 12:54:40 +0100

    bunp dependencies

---
+++ src/state.rs
@@ -0,0 +1,23 @@
+use cw_storage_plus::Item;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{CanonicalAddr, StdResult, Storage};
+
+pub static CONFIG: Item<Config> = Item::new("config");
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
+pub struct Config {
+    pub owner: CanonicalAddr,
+    pub message: String,
+}
+
+impl Config {
+    pub fn read_config(storage: &dyn Storage) -> StdResult<Config> {
+        CONFIG.load(storage)
+    }
+
+    pub fn save_config(&self, storage: &mut dyn Storage) -> StdResult<()> {
+        CONFIG.save(storage, &self)
+    }
+}
